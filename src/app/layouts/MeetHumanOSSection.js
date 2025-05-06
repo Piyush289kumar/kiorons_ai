ca95ccea2d425d42f90b668adcf2cd2c17501484
Piyush289kumar <piyushraikwar289@gmail.com>
2025-05-06 18:35:41 +0530

    Replace Components

--- src/app/layouts/MeetHumanOSSection.js
+++ src/app/layouts/MeetHumanOSSection.js
@@ -5,34 +5,27 @@ import { OrbitingCircles } from '@/components/magicui/orbiting-circles'
 import { File, Settings, Search } from 'lucide-react'
 import FadeUpSection from '@/components/animation/FadeUpSection'
 
+import { BentoCard, BentoGrid } from '@/components/magicui/bento-grid'
+import { BentoDemo } from '@/components/landing/BentoGrid'
+
 const dataSet = ['Born.', 'Organized.', 'Designed.', 'and Launched.']
 
 export default function MeetHumanOSSection() {
   return (
     <section className="w-full max-w-4xl mx-auto px-4 py-12 md:py-24 font-gellix">
       {/* Heading */}
-      <div className="flex flex-col items-center text-center">
-        <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold  font-gellix">Meet</h1>
+      <div className="flex flex-col items-center text-center mb-14">
+        <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold  font-gellix">
+          Meet
+        </h1>
         <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold leading-10 font-gellix">
           Human<span className="gradient-text-4 font-gellix">OS</span>
         </h1>
       </div>
-      
+
       {/* Orbiting Icons */}
       <FadeUpSection>
-        <div className="relative overflow-hidden h-[300px] sm:h-[400px] md:h-[500px] w-full my-8 md:my-12">
-          <OrbitingCircles className="scale-75 sm:scale-90 md:scale-100">
-            <File className="w-6 h-6 sm:w-8 sm:h-8" />
-            <Settings className="w-6 h-6 sm:w-8 sm:h-8" />
-            <File className="w-6 h-6 sm:w-8 sm:h-8" />
-          </OrbitingCircles>
-          <OrbitingCircles radius={100} reverse className="scale-75 sm:scale-90 md:scale-100">
-            <File className="w-6 h-6 sm:w-8 sm:h-8" />
-            <Settings className="w-6 h-6 sm:w-8 sm:h-8" />
-            <File className="w-6 h-6 sm:w-8 sm:h-8" />
-            <Search className="w-6 h-6 sm:w-8 sm:h-8" />
-          </OrbitingCircles>
-        </div>
+        <BentoDemo />
       </FadeUpSection>
 
       {/* Description and Data List */}
