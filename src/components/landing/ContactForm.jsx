ab946da31a8573b7cf138596ac74e1ee03a4ff1c
Piyush289kumar <piyushraikwar289@gmail.com>
2025-05-07 17:29:20 +0530

    Deploy V1

--- src/components/landing/ContactForm.jsx
+++ src/components/landing/ContactForm.jsx
@@ -22,8 +22,8 @@ export function ContactCard() {
     <Card className="p-0 max-w-sm w-full shadow-none border-none">        
         <MagicCard gradientColor={theme === 'dark' ? '#262626' : '#D9D9D955'} className="p-0">
           <CardHeader className="border-b border-border p-4 [.border-b]:pb-4">
-            <CardTitle>Login</CardTitle>
-            <CardDescription>Enter your credentials to access your account</CardDescription>
+            <CardTitle>Get Early Access</CardTitle>
+            <CardDescription>Join now to get early access to the beta version!</CardDescription>
           </CardHeader>
           <CardContent className="p-4">
             <form>
@@ -40,7 +40,7 @@ export function ContactCard() {
             </form>
           </CardContent>
           <CardFooter className="p-4 border-t border-border [.border-t]:pt-4">
-            <Button className="w-full">Sign In</Button>
+            <Button className="w-full">Join Early</Button>
           </CardFooter>
         </MagicCard>
       </Card>
