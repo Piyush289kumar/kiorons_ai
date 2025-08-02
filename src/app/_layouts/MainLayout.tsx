707bb21a5685db02659c13f969015c52076e30e2
Piyush289kumar <piyushraikwar289@gmail.com>
2025-08-02 16:54:53 +0530

    Update MainLayout and globals.css to use 'Gellix' font for improved typography

--- src/app/_layouts/MainLayout.tsx
+++ src/app/_layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../_components/Navbar";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-gellix">
       <Navbar />
       <main className="">{children}</main> {/* add padding only if navbar is fixed */}
       {/* <Footer /> */}
